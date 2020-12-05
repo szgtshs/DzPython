61be356388e136e0008ccc29abc64e5372df86ba
markant <4916867@mail.ru>
2020-12-05 10:04:38 +0300

    dz1

---
+++ dz1/dz11.py
@@ -0,0 +1,10 @@
+
+x = 1
+y = 2
+print("Переменные x и y -", x, y)
+page_1 = int(input('Введите страницу '))
+paragraph_1 = int(input('Введите параграф '))
+page_2 = int(input('Введите следующую страницу '))
+paragraph_2 = int(input('Введите следующий параграф '))
+print('Первая страница', page_1, 'Параграф', paragraph_1, 'Следующая страница', page_2, 'Параграф', paragraph_2)
+
